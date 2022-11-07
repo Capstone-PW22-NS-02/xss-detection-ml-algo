eb65501bb104a97e57a480f80f3474b85a2e8d9b
SuryaMN <suryamnnitk@gmail.com>
2022-11-07 13:11:55 +0530

    Added cors

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 import numpy as np
 import pickle
-
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST'])
+@cross_origin()
 def fun():
 
     product = request.json
@@ -21,4 +22,4 @@ def fun():
     
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(port=5000,debug=True)
